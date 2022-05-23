f664582b3d1403ed1b50d262f0e91939ecdb9985
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2022-05-23 15:41:20 +0100

    latest commit

--- src/js/model.js
+++ src/js/model.js
@@ -1,7 +1,7 @@
 import { async } from 'regenerator-runtime';
 import { API_URL, RES_PER_PAGE, KEY } from './config.js';
 // import { getJSON, sendJSON } from './helpers.js';
-import { AJAX } from './helpers.js';
+import { AJAX } from './helper.js';
 
 export const state = {
   recipe: {},
